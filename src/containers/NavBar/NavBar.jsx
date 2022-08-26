fb417a51432deb8f7e1bd73c6be784fccd9517e8
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-08-26 20:37:33 +0100

    added props and components

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -3,11 +3,12 @@ import styles from './NavBar.module.scss'
 import SearchBar from "../../components/SearchBar"
 import Filters from "../../components/Filters"
 
-const NavBar = () => {
+const NavBar = (props) => {
+  const { updateSearchTerm,  setAbvFilterOn, abvFilterOn, setClassicFilterOn, classicFilterOn, setAcidicFilterOn, acidicFilterOn } = props;
   return (
     <div className={styles.navContainer}>
-      <SearchBar />
-      <Filters />
+      <SearchBar updateSearchTerm={updateSearchTerm} />
+      <Filters updateSearchTerm={updateSearchTerm} abvFilterOn={abvFilterOn} setAbvFilterOn={setAbvFilterOn} classicFilterOn={classicFilterOn} setClassicFilterOn={setClassicFilterOn} acidicFilterOn={acidicFilterOn} setAcidicFilterOn={setAcidicFilterOn}/>
     </div>
   )
 }
