7abd8a42c966448ed53ece39484277876a707f76
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-08-26 20:35:59 +0100

    setting up functions

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,37 +1,62 @@
 import React from 'react'
 import styles from "./Filters.module.scss"
 
-const Filters = () => {
-  const filterItems = [
-    {
-      id: "abv",
-      label:"High ABV (>6.0%)"
-    },
-    {
-      id: "classic",
-      label: "Classic Range"
-    },
-    {
-      id:"acidic",
-      label: "Acidic (ph <4)"
-    }
-  ]
+const Filters = (props) => {
+const { setAbvFilterOn, abvFilterOn, setClassicFilterOn, classicFilterOn, setAcidicFilterOn, acidicFilterOn } = props;
+  // const filterItems = [
+  //   // {
+  //   //   id: "abv",
+  //   //   label:"High ABV (>6.0%)",
+  //   //   filter:abvFilterOn,
+  //   //   setFilter: setAbvFilterOn
+  //   // },
+  //   {
+  //     id: "classic",
+  //     label: "Classic Range",
+  //     filter:classicFilterOn,
+  //     setFilter: setClassicFilterOn
+  //   },
+  //   {
+  //     id:"acidic",
+  //     label: "Acidic (ph <4)",
+  //     filter:acidicFilterOn,
+  //     setFilter: setAcidicFilterOn
+  //   }
+  // ]
  
-  const createFilters = filterItems.map((filterItem) => {
-    return (
-      <>
-      <div className={styles.filterItem}>
-        <label for={filterItem.id}>{filterItem.label}</label>
-        <input id={filterItem.id} type="checkbox" />
-      </div>
-      </>
-    )
-  })
+  // const createFilters = filterItems.map((filterItem) => {
+  //   return (
+  //     <>
+  //     {/* <div className={styles.filterItem} id={filterItem.id}>
+  //       <label for={filterItem.id}>{filterItem.label}</label>
+  //       <input id={filterItem.id} type="checkbox" onClick={filterItem.setFilter(!filterItem.filter)}/>
+  //     </div> */}
+
+  //     <div className={styles.filterItem}>
+  //       <label for="abv">High ABV (>6.0%) </label>
+  //       <input id="abv" type="checkbox" onClick={() => {setAbvFilterOn(!abvFilterOn)}}/>
+  //     </div>
+  //     </>
+  //   )
+  // })
 
   return (
-    <div> 
-    {createFilters}
-    </ div>
+    <>
+       <div className={styles.filterItem}>
+        <label for="abv"> High ABV (>6.0%) </label>
+        <input id="abv" type="checkbox" onChange={() => setAbvFilterOn(!abvFilterOn)}/>
+      </div>
+
+      <div className={styles.filterItem}>
+        <label for="classic"> Classic Range </label>
+        <input id="classic" type="checkbox" onChange={() => setClassicFilterOn(!classicFilterOn)}/>
+      </div>
+
+      <div className={styles.filterItem}>
+        <label for="acidic"> Acidic (pH below 4%) </label>
+        <input id="acidic" type="checkbox" onChange={() => setAcidicFilterOn(!acidicFilterOn)}/>
+      </div>
+      </>
   )
 
 }
