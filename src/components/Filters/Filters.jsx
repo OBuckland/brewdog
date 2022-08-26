8f8f65b6aa2aff98346c511b5f55265058361794
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-08-26 13:48:23 +0100

    add filter content

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import styles from "./Filters.module.scss"
 
 const Filters = () => {
+  const filterItems = [
+    {
+      id: "abv",
+      label:"High ABV (>6.0%)"
+    },
+    {
+      id: "classic",
+      label: "Classic Range"
+    },
+    {
+      id:"acidic",
+      label: "Acidic (ph <4)"
+    }
+  ]
+ 
+  const createFilters = filterItems.map((filterItem) => {
+    return (
+      <>
+      <div className={styles.filterItem}>
+        <label for={filterItem.id}>{filterItem.label}</label>
+        <input id={filterItem.id} type="checkbox" />
+      </div>
+      </>
+    )
+  })
+
   return (
-    <div>Filters</div>
+    <div> 
+    {createFilters}
+    </ div>
   )
+
 }
 
 export default Filters
