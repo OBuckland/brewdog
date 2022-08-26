6cfca53937108d725cc340640318144e50d9cf9d
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-08-26 13:12:51 +0100

    import components to NavBar

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from './NavBar.module.scss'
+import SearchBar from "../../components/SearchBar"
+import Filters from "../../components/Filters"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className={styles.navContainer}>
+      <SearchBar />
+      <Filters />
+    </div>
   )
 }
 
