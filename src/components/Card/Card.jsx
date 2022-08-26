8d051b356f40a0fdee78f64877671293c1e06baa
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-08-26 13:13:37 +0100

    set up Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import styles from "./Card.module.scss"
 
-const Card = () => {
+
+
+const Card = (props) => {
+  const {image_url, name, description} = props.beer;
   return (
-    <div>Card</div>
+    <>
+      <div className={styles.card}>
+        <img className={styles.image} src={image_url} alt={name} />
+        <h4 className={styles.title}>{name}</h4>
+        <p>{description}</p>
+      </div>
+    </>
   )
+
 }
 
 export default Card
