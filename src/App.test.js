203b97c71576fbfbca2869e8bca0672bd9efc589
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-08-27 17:07:08 +0100

    write tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { shallow } from "enzyme";
+import React from "react";
+
 import App from "./App";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe("App tests", () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<App />);
+  });
+
+  it("should render", () => {
+    expect(component).toBeTruthy();
+  });
+
+  //   it("should render a button", () => {
+  //     expect(component.find("button").length).toEqual(1);
+  //   });
+});
