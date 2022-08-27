d9d04e8e69e3a29c719a8733072ab20426ff48aa
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-08-27 13:05:03 +0100

    btn added

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -4,11 +4,16 @@ import SearchBar from "../../components/SearchBar"
 import Filters from "../../components/Filters"
 
 const NavBar = (props) => {
-  const {updateAbvFilterOn, updateSearchTerm, updateClassicFilterOn, updateAcidicFilterOn, abvFilterOn, classicFilterOn, acidicFilterOn } = props;
+  const { updateAbvFilterOn, updateSearchTerm, updateClassicFilterOn, updateAcidicFilterOn, abvFilterOn, classicFilterOn, acidicFilterOn } = props;
+  const resetFilters = () => {
+    window.location.reload();
+  }
+
   return (
     <div className={styles.navContainer}>
       <SearchBar updateSearchTerm={updateSearchTerm} />
       <Filters updateAbvFilterOn={updateAbvFilterOn} updateSearchTerm={updateSearchTerm} updateClassicFilterOn={updateClassicFilterOn} updateAcidicFilterOn={updateAcidicFilterOn} abvFilterOn={abvFilterOn} classicFilterOn={classicFilterOn} acidicFilterOn={acidicFilterOn} />
+      <div><button onClick={() => {resetFilters()}}>Reset</button></div>
     </div>
   )
 }
