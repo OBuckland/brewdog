4be0474e7f4744abcb74b862ea6423ead728e9ea
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-08-29 10:05:07 +0100

    tidy file

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import styles from './NavBar.module.scss'
-import SearchBar from "../../components/SearchBar"
-import Filters from "../../components/Filters"
+import React from "react";
+import styles from "./NavBar.module.scss";
+import SearchBar from "../../components/SearchBar";
+import Filters from "../../components/Filters";
 
 const NavBar = (props) => {
-  const { updateAbvFilterOn, updateSearchTerm, updateClassicFilterOn, updateAcidicFilterOn, abvFilterOn, classicFilterOn, acidicFilterOn } = props;
+  const {
+    updateAbvFilterOn,
+    updateSearchTerm,
+    updateClassicFilterOn,
+    updateAcidicFilterOn,
+    abvFilterOn,
+    classicFilterOn,
+    acidicFilterOn,
+  } = props;
+
   const resetFilters = () => {
     window.location.reload();
-  }
+  };
 
   return (
     <div className={styles.navContainer}>
       <SearchBar updateSearchTerm={updateSearchTerm} />
-      <Filters updateAbvFilterOn={updateAbvFilterOn} updateSearchTerm={updateSearchTerm} updateClassicFilterOn={updateClassicFilterOn} updateAcidicFilterOn={updateAcidicFilterOn} abvFilterOn={abvFilterOn} classicFilterOn={classicFilterOn} acidicFilterOn={acidicFilterOn} />
-      <div><button onClick={() => {resetFilters()}}>Reset</button></div>
+      <Filters
+        updateAbvFilterOn={updateAbvFilterOn}
+        updateSearchTerm={updateSearchTerm}
+        updateClassicFilterOn={updateClassicFilterOn}
+        updateAcidicFilterOn={updateAcidicFilterOn}
+        abvFilterOn={abvFilterOn}
+        classicFilterOn={classicFilterOn}
+        acidicFilterOn={acidicFilterOn}
+      />
+      <div className={styles.buttonContainer}>
+        <button
+          onClick={() => {
+            resetFilters();
+          }}
+        >
+          Reset
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
