c42fe7a5acc71f67314c6d098382f35450d6dcab
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-08-29 10:04:06 +0100

    add p tags for ABV & tagline

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import styles from "./Card.module.scss"
-
-
+import React from "react";
+import styles from "./Card.module.scss";
 
 const Card = (props) => {
-  const {image_url, name, description} = props.beer;
+  const { image_url, name, tagline, abv } = props.beer;
   return (
     <>
       <div className={styles.card}>
         <img className={styles.image} src={image_url} alt={name} />
         <h4 className={styles.title}>{name}</h4>
-        <p>{description}</p>
+        <p>{abv} ABV</p>
+        <p>"{tagline}"</p>
       </div>
     </>
-  )
+  );
+};
 
-}
-
-export default Card
+export default Card;
